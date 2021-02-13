604511293e37b54ee98cfa271500a2950d798afd
moises orellana <moorellana@unah.hn>
2021-02-12 22:45:29 -0600

    Tarea3-ClaseBL-DatosdePrueba

---
+++ Win.TiendaElectronicos/FormProductos.cs
@@ -0,0 +1,27 @@
+﻿using BL.Tecnologia;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.TiendaElectronicos
+{
+    public partial class FormProductos : Form
+    {
+        ProductosBL Productos;
+
+        public FormProductos()
+        {
+            InitializeComponent();
+
+            Productos = new ProductosBL();
+
+            listaProductosBindingSource.DataSource = Productos.ObtenerProductos();
+        }
+    }
+}
